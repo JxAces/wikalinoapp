b2cdbb3681881f207579e689669217d17587b764
John Mark <johnmarkbolongan8@gmail.com>
2026-04-23 20:01:24 +0800

    Faster Image

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,38 @@
+import { Asset } from "expo-asset";
 import { Stack } from "expo-router";
+import * as SplashScreen from "expo-splash-screen";
+import { useEffect, useState } from "react";
+
+SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
+  const [ready, setReady] = useState(false);
+
+  useEffect(() => {
+    async function preloadAssets() {
+      try {
+        await Asset.loadAsync([
+          require("../assets/images/pageBackground/HomePage.png"),
+          require("../assets/images/pageBackground/LandingPage.png"),
+          require("../assets/images/pageBackground/ThirdPage.png"),
+        ]);
+      } catch (e) {
+        console.log("Preload error:", e);
+      } finally {
+        setReady(true);
+        await SplashScreen.hideAsync();
+      }
+    }
+
+    preloadAssets();
+  }, []);
+
+  // Prevent app from rendering until images are ready
+  if (!ready) return null;
+
   return (
     <Stack>
       <Stack.Screen name="landing" options={{ headerShown: false }} />
-
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
   );
